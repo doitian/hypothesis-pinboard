90093497ba487454d4243344a22c889f8f6a8a06
ian <me@iany.me>
2022-04-15 23:07:37 +0800

    first version for test

--- index.js
+++ index.js
@@ -2,29 +2,44 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-/**
- * Given https://hypothes.is/a/RjuHSrr0Eey-hmukl1Mumg
- * Return https://hypothes.is/api/annotations/RjuHSrr0Eey-hmukl1Mumg
- */
-function getAPIEndpoint(entryURL) {
-  let parts = entryURL.split('/')
-}
-
 function respondWithJSON(bodyObject) {
   return new Response(JSON.stringify(bodyObject, null, 2), {
     headers: { 'content-type': 'application/json;charset=UTF-8' },
   })
 }
 
-function runTest() {
-  if (
-    getAPIEndpoint('https://hypothes.is/a/RjuHSrr0Eey-hmukl1Mumg') !==
-    'https://hypothes.is/api/annotations/RjuHSrr0Eey-hmukl1Mumg'
-  ) {
-    throw 'getAPIEndpoint Error'
-  }
+// Given https://hypothes.is/a/RjuHSrr0Eey-hmukl1Mumg
+// Return https://hypothes.is/api/annotations/RjuHSrr0Eey-hmukl1Mumg
+function getAnnotationAPIEndpoint(annotationURL) {
+  const parts = annotationURL.split('/')
+  parts[3] = 'api/annotations'
+  return parts.join('/')
+}
 
-  return respondWithJSON({ test: 'pass' })
+async function fetchAnnotation(annotationURL) {
+  return fetch(getAnnotationAPIEndpoint(annotationURL), {
+    headers: {
+      authorization: `Bearer ${HYPOTHESIS_TOKEN}`,
+      accept: 'application/json',
+      'content-type': 'application/json',
+    },
+  })
+}
+
+async function addBookmark(annotation) {
+  const params = URLSearchParams({
+    auth_token: PINBOARD_TOKEN,
+    url: annotation.uri,
+    description: annotation.document.title[0],
+    replace: 'no',
+  })
+  const resp = await fetch(`https://api.pinboard.in/v1/posts/add?${params}`, {
+    headers: {
+      accept: 'application/json',
+    },
+  })
+
+  return respondWithJSON({ duplicated: !resp.ok })
 }
 
 /**
@@ -33,11 +48,15 @@ function runTest() {
  */
 async function handleRequest(request) {
   const url = new URL(request.url)
+  console.log(url)
 
-  if (url.pathname === '/test') {
-    return runTest(request)
+  // https://hypothesis-pinboard.doitian.workers.dev/https://hypothes.is/a/RjuHSrr0Eey-hmukl1Mumg
+  const annotationURL = `https://${url.pathname.split(/:\/\/?/, 2)[1]}`
+  const annotation = await fetchAnnotation(annotationURL)
+
+  if (request.method == 'GET') {
+    return annotation
   }
 
-  let body = await request.json()
-  return respondWithJSON({ pong: body })
+  return addBookmark(annotation)
 }
